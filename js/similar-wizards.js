9e9e73808df79e2e4a65029f4889ca6d0c1dc5d9
Nadezhda Obidina <nadezhdaobidina@rambler.ru>
2017-12-17 15:32:42 +0300

    поправила название функции, убрала updateWizards из window

--- js/similar-wizards.js
+++ js/similar-wizards.js
@@ -33,12 +33,12 @@
 
   window.changeCoatColorHandler = function (color) {
     window.mainWizardCoatColor = color;
-    window.debounce(window.updateWizards);
+    window.debounce(updateWizards);
   };
 
   window.changeEyesColorHandler = function (color) {
     window.mainWizardEyesColor = color;
-    window.debounce(window.updateWizards);
+    window.debounce(updateWizards);
   };
 
   var getRank = function (wizard) {
@@ -54,7 +54,7 @@
     return rank;
   };
 
-  var namesComparator = function (left, right) {
+  var compareNames = function (left, right) {
     if (left > right) {
       return 1;
     } else if (left < right) {
@@ -64,11 +64,11 @@
     }
   };
 
-  window.updateWizards = function () {
+  var updateWizards = function () {
     renderWizards(wizards.sort(function (left, right) {
       var rankDiff = getRank(right) - getRank(left);
       if (rankDiff === 0) {
-        rankDiff = namesComparator(left.name, right.name);
+        rankDiff = compareNames(left.name, right.name);
       }
       return rankDiff;
     }));
@@ -76,7 +76,7 @@
 
   var successHandler = function (data) {
     wizards = data;
-    window.updateWizards();
+    updateWizards();
   };
 
   window.backend.load(successHandler, window.backend.errorHandler);
